dbbc1d963bc9369185d860e5ad05c316b014edce
conekilla69 <wrcfan@verizon.net>
2015-11-09 03:56:24 +0000

    cannot figure it out beyond class instance.

---
+++ chessmaster.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#-*- coding: utf-8: -*-
+"""Chess module"""
+
+
+import time
+
+class ChessPiece(object):
+    
+    def __init__(self, position, prefix, moves):
+        self.position
+        self. prefix = ('.')
+        self.moves
+        
+        def __init__(self, position):
+            return position
+            position = ([''])
+            
+        def __init__(self, moves):
+            return moves
+            moves = [('.'] 
+
+        def algebraic_to_numeric(tile):
+            for values in tile:
+            x = range(8)
+            y = range(8)
+        return algebraic_to_numeric
+        tile = [['.']*8for_inrange(8)]
+    
+        def is_legal_move(position):
+                     
+        if is_legal_move = algebraic_to_numeric:
+        return True
+
+        else:
+        False
+        excep = '{}' is not a legal start position
+        return raise ValueError(except.format(postion))
+
+
+class Rook(ChessPiece):
+    def __init__(self, prefix):
+        self.prefix = 'R'
+    def is_legal_move(R):
+    
+####question3####
+
+class Bishop(ChessPiece):
+    def __init__(self, prefix):
+        self.prefix = 'B'
+        def is_legal_move():
+
+####question4####
+
+class King(ChessPiece):
+    def __init__(self, prefix):
+        self.prefix = 'K'
+        def is_legal_move():
+
+#####Question 5#####
+
+class ChessMatch(object):
+    def __init__(self, pieces, log):
+        self.pieces = '{}'
+        self.log = []
